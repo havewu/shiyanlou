19ad6092e6616dc6eb9ed391c584bc4b080a4127
havewu <havewu@users.noreply.github.com>
2017-10-30 18:01:39 +0800

    Update Pointer3.c

--- c/Pointer3.c
+++ c/Pointer3.c
@@ -2,6 +2,7 @@
 int main()
 {
  int n,i,k,*p;//n : people number; k:n--;
+ int m=3;
  int a[3]={11,22,33};
  printf("Enter the number of people:");
  scanf("%d",&n);
@@ -12,11 +13,39 @@ int main()
 
  k=n;
 
-  for(p=arr;p<(arr+n);p++)
-    printf("%p,*p=%d\n",p,*p);
+
 // while(k>0)
 // {
     
 // }
  return 0;
+}
+
+void for()
+{
+ for(p=arr;p<(arr+n);p++)
+ {
+   if(*p == 0)
+   {
+     continue;
+   }
+   if(*p%m==0)
+   {
+     *p = 0;
+     k--;
+   }
+   if(p == arr+n)
+   {
+     
+   }
+ }
+}
+void fuzhi()
+{
+ int i;
+  for(i=0;i<n;i++)
+  {
+    if(*p == 0)continue;
+    *p = *(p-1) + 1;
+  }
 }
