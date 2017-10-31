289a6c856c44660fb5b920cc5d55dc704d9add90
da <593990469@qq.com>
2017-10-31 16:08:00 +0800

    End the funcion of  pointer3.c

--- c/Pointer3.c
+++ c/Pointer3.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main()
 {
- int n,i,k,*p;//n : people number; k:n--;
+ int n,i,j,*p;//n : people number; k:n--;
  int m=3;
- int a[3]={11,22,33};
  printf("Enter the number of people:");
  scanf("%d",&n);
  printf("n=%d\n",n);
@@ -11,8 +10,8 @@ int main()
   for(i=0;i<n;i++)
    arr[i]= i+1;
 
- k=n;
-
+ int k=n;
+printf("0 k=%d\n",k);
 
 // while(k>0)
 // {
@@ -20,28 +19,47 @@ int main()
 // }
 // for(p=arr;p<(arr+n);p++)
  p=arr;
- for(i=1;k>0;i++)
+printf("------begin for-------\n");
+ for(i=1;k>1;i++)
  {
-   if(*p == 0 && p == arr+n)
-   {
-     p = arr;
-   }
+printf("--------i=%d--------",i);
+printf("k=%d\n",k);
+printf("*p=%d\n",*p);
+  //if(*p == 0 && p == (arr+n-1))
+  //{
+  //  p = arr;
+  //}
    if(*p == 0)
    {
-     while(*p = 0)
+     while(*p == 0)
+     {
+       if(p == (arr + n-1))
+       {
+         p=arr;
+         continue;
+       }
        p++;
-     *p = i;
+     }
+      *p = i;
    }
    else
    {
      *p = i;
    }
-   if(*p%m==0)
+
+printf("-----\n");
+ for(j=0;j<n;j++)
+  { printf("arr[%d]=%d\n",j,arr[j]);}
+printf("k=%d\n",k);
+printf("*p=%d\n",*p);
+
+   if(*p != 0 && *p%m==0)
    {
      *p = 0;
      k--;
    }
-   if(p == arr+n)
+   if(p == (arr+n-1))
+   //if(p == &arr[n])
    {
      p=arr;
    }
@@ -49,10 +67,17 @@ int main()
    {
      p++;
    }
+printf("-----\n");
+ for(j=0;j<n;j++)
+  { printf("arr[%d]=%d\n",j,arr[j]);}
+printf("k=%d\n",k);
+printf("*p=%d\n",*p);
  }
+ for(i=0;i<n;i++)
+   printf("i=%d,arr=%d\n",i,arr[i]);
  //for(p=arr;p<(arr+n);p++)
  for(p=arr,i=0;i<n;i++,p++)
- if(*p != 0 )printf("THE END:%d\n",i);
+   if(*p != 0 )printf("THE END:%d\n",i+1);
  return 0;
 }
 // void fuzhi()
