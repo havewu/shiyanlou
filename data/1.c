df071a27e7c9c919343caf0477214cdeaea083ba
da <593990469@qq.com>
2017-11-13 11:50:41 +0800

    edit data/1.c

--- data/1.c
+++ data/1.c
@@ -2,26 +2,25 @@
 
 int main()
 {
-  int maxsum(int arr[]);
-  int sum;
+  int maxsum(int arr[],int n);
+  int n,sum;
   int arr1[]={-2,11,-4,13,-5,2,-5,-3,12,-9};
   int arr2[]={0,-3,6,8,-20,21,8,-9,10,-1,3,6,5};
-  sum=sizeof(arr1)/sizeof(arr1[0]);
-  printf("arr1,len:%d\n",sum);
-  sum=sizeof(arr2)/sizeof(arr2[0]);
-  printf("arr2,len:%d\n",sum);
-  
-  sum = maxsum(arr1);
+
+  n=sizeof(arr1)/sizeof(arr1[0]);
+  sum = maxsum(arr1,n);
   printf("arr1,maxsum:%d\n",sum);
-  sum = maxsum(arr2);
+
+  n=sizeof(arr2)/sizeof(arr2[0]);
+  sum = maxsum(arr2,n);
   printf("arr2,maxsum:%d\n",sum);
 
+  return 0;
 }
 
-int maxsum(int arr[])
+int maxsum(int arr[],int n)
 {
-  int n,i,j,max=0,sum=0;
-  n=sizeof(arr)/sizeof(arr[0]);
+  int i,j,max=0,sum=0;
   printf("arr,len:%d\n",n);
   for(i=0;i<n;i++)
   { 
@@ -30,10 +29,11 @@ int maxsum(int arr[])
     for(j=i;j<n;j++)
     {
       sum = sum + arr[j];
-      if(max < sum) max =sum;
+      if(max < sum)
+      {
+         max =sum;
+      }
     }
   }
-
-
   return max;
 }
